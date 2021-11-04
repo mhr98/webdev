30628c177335a920b4638823a7aea9e08d3b2335
mohammad <mohamm.ad@outlook.com>
2021-11-04 20:18:48 +0300

    adding wiki project

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -8,3 +8,6 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def wiki(request,title):
+    return render(request,'encyclopedia/wiki.html',{'wiki':util.get_entry(title),"tilte":title})
+
